752ba9170b620d697744b868afc222e341ec307b
BA <hrbbrb@Brkdrns-MacBook-Pro.local>
2020-05-01 21:40:35 +0200

    final

---
+++ build/precache-manifest.550f18759ef509a502abaa602d21713f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d997b035ab019c5ed38b3686a59e211",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a78bad21c94009059ed3",
+    "url": "./static/css/main.e9cb0a5d.chunk.css"
+  },
+  {
+    "revision": "894daed5cac13c915747",
+    "url": "./static/js/2.ee16ff66.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ee16ff66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a78bad21c94009059ed3",
+    "url": "./static/js/main.66cc85f5.chunk.js"
+  },
+  {
+    "revision": "821e3e9e8623f25acdce",
+    "url": "./static/js/runtime-main.62724d6a.js"
+  }
+]);
